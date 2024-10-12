ad62f10d64efb89f795bed34dde716c8becd4cb3
eaoyama <e.h.aoyama@gmail.com>
2024-10-11 22:19:26 -0700

    added Jenkinsfile

--- src/test/java/com/example/spring_boot_docker/SpringBootDockerApplicationTests.java
+++ src/test/java/com/example/spring_boot_docker/SpringBootDockerApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.spring_boot_docker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SpringBootDockerApplicationTests {
@@ -9,5 +10,11 @@ class SpringBootDockerApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	@Test
+	void simpleTest() {
+		assertTrue(true);
+	}
+
 
 }
